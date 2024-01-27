36bf1d739f176ca5709b7394a4180d2b96ba96a4
MarcoGarciaF <mgarciaf1705@alumno.ipn.mx>
2024-01-27 03:13:50 -0600

    (+) Actualización de README de CdeO

--- CdeOtros6/ModifiedCode/Codigo6.java
+++ CdeOtros6/ModifiedCode/Codigo6.java
@@ -8,66 +8,47 @@
 
 package com.generation;
 
-import java.util.Scanner;
+import java.util.Scanner; // Se importó la librería de Scanner, ya que en le código se estaba utilizando.
 
 public class Codigo6 { // Se cambio el nombre de la clase siguiendo la estructura CamelCase
 
     public static void main(String[] args) {
     	
-        // Se pasaron las declaraciones de las variables al inicio del programa
-    	
-        int[] numeros = new int[20]; // Se modificó la sintaxos del arrehlo
-        
-        int opcion = 0;
-        
-        int multiplo = 0;
-        
-        Scanner scan = new Scanner(System.in);
+        int[] n = new int[20];
 
-        System.out.println("¿Qué números quiere resaltar? "); // Se agregó una 'n' faltante y se quito '\n'
+        for (int i = 0; i < 20; i++) {
+        	
+            n[i] = (int) (Math.random() * 381) + 20;
+            
+            System.out.print(n[i] + " ");
+            
+        }
+
+        System.out.println("\n¿Qué números quiere resaltar?");
         
         System.out.print("(1 – los múltiplos de 5, 2 – los múltiplos de 7): ");
+
+        Scanner scanner = new Scanner(System.in);  // Se agregó un Scanner y se quito ' int opcion = Integer.parseInt(System.console().readLine(); ' ya que Scanner nos fa un mejor rendimiento en el programa.
         
-        opcion = Integer.parseInt(scan.nextLine()); // Se agregó un paréntesis y 'scan'
+        int opcion = scanner.nextInt();  // Se agrega esta linea para la entrada de datos del usuario
 
-        multiplo = (opcion == 1) ? 5 : 7; // Se corrigió la sintaxis de está operación
+        int multiplo = (opcion == 1) ? 5 : 7; // Se intercambio de lugares las variables '?' y ':'
 
-        GenerarNumerosAleatorios(numeros); // Se cambió el nombre de la función según el programa
-
-        compararMultiplos(numeros, multiplo); // Se agregó esta función
-
-        scan.close();
-    }
-
-    private static void compararMultiplos(int[] numeros, int multiplo) {
-    	
-        for (int numero : numeros) {
+        for (int e : n) { // En lugar de char, recibimos un dato entero 'int' y cambiamos el bucle 'foreach' por un bucle 'for'
         	
-            if (numero % multiplo == 0) { // Se modificó la sintaxis del ciclo 'if' para el funcionamiento del programa
+            if (e % multiplo == 0) {
             	
-            	System.out.print(" [ " + numero + " ] ");
-            	
+                System.out.print(" [ " + e + " ] ");
+                
             } else {
             	
-            	System.out.print(numero + " "); // Se corrigió esta impresion en la sintaxis
-            	
+                System.out.print(e + " ");
+                
             }
             
         }
-        
-    }
-    
-    private static void GenerarNumerosAleatorios(int[] numeros) {
-    	
-        for (int i = 0; i < numeros.length; i++) { // Se corrigio la sintaxis del ciclo 'for' para el funcionamiento adecuado
-        	
-            numeros[i] = (int) (Math.random() * 381) + 20;
-            
-            System.out.print(numeros[i] + " "); // Se agrego una impresión
-            
-        }
-        
-        System.out.println(); // Se agrego y corrigio 'println'
+
+        scanner.close();  // Se cerró el scanner
         
     }
     
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 En este repositorio estan las revisiones de los códigos de otros
+
+Los archivos donde están las evaluaciones son:
+
+liveCoding1: CdeOtros1 > ModifiedCode > index.html
+
+liveCoding2: CdeOtros2 > ModifiedCode > formulario.html
+
+liveCoding3: CdeOtros3 > ModifiedCode > filtro.html
+
+liveCoding4: CdeOtros4 > ModifiedCode > Codigo4.java
+
+liveCoding5: CdeOtros5 > ModifiedCode > Codigo5.java
+
+liveCoding6: CdeOtros6 > ModifiedCode > Codigo6.java
